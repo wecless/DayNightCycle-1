fb2f188670533873cbf8748351ff15be8f12b2c1
Dave Hards <unstablesun@gmail.com>
2016-02-24 21:15:54 -0800

    sun and moon on timer

--- Assets/DayNightCycle.cs
+++ Assets/DayNightCycle.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DayNightCycle : MonoBehaviour {
+public class DayNightCycle : MonoBehaviour 
+{
+
+	public GameObject sunLight = null;
+	public GameObject moonLight = null;
+	public float dayNightSpeed = 0.01f;
+
+	public bool rot_x_axis = false;
+	public bool rot_y_axis = false;
+	public bool rot_z_axis = false;
+	public bool rot_direction = false;
+
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
+		//Rotation--------------------------------------------------------------
+		if(rot_y_axis == true)
+		{
+			if(rot_direction == false)
+			{
+				sunLight.transform.Rotate(Vector3.up * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.up * Time.deltaTime * dayNightSpeed);
+			}
+			else
+			{
+				sunLight.transform.Rotate(Vector3.down * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.down * Time.deltaTime * dayNightSpeed);
+			}
+		}
+
+		if(rot_x_axis == true)
+		{
+			if(rot_direction == false)
+			{
+				sunLight.transform.Rotate(Vector3.right * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.right * Time.deltaTime * dayNightSpeed);
+			}
+			else
+			{
+				sunLight.transform.Rotate(Vector3.left * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.left * Time.deltaTime * dayNightSpeed);
+			}
+		}
+
+		if(rot_z_axis == true)
+		{
+			if(rot_direction == false)
+			{
+				sunLight.transform.Rotate(Vector3.forward * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.forward * Time.deltaTime * dayNightSpeed);
+			}
+			else
+			{
+				sunLight.transform.Rotate(Vector3.back * Time.deltaTime * dayNightSpeed);
+				moonLight.transform.Rotate(Vector3.back * Time.deltaTime * dayNightSpeed);
+			}
+		}
+		//----------------------------------------------------------------------
+
 	}
 }
